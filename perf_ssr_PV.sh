1e3d97793c2aa878814e43e6f14d34a9a75e22b7
dcarru01 <73667554+dcarru01@users.noreply.github.com>
2025-09-05 12:06:52 -0400

    Rename perf_ssr_PV.sh to bin/perf_ssr_PV.sh

--- perf_ssr_PV.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-module load CCEnv StdEnv/2020
-source ~/.virtualenvs/perf_ssr/bin/activate
-
-parallel -a final_continent_acc.txt PERF -i phase/genomes/{}.fna -o phase/perf_out/{}_perf.tsv -g phase/gff/{}.gff --gene-key locus_tag
